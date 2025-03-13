c56eb11a322c3f556fa8fbd1b32b7cc4d22f1a1b
dato-svg <139239441+dato-svg@users.noreply.github.com>
2025-03-13 15:11:04 +0400

    save
    save

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public float Health = 70f;
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+        //don't play audio for small damages
+        if (damage >= 10)
+            GetComponent<AudioSource>().Play();
+        //decrease health according to magnitude of the object that hit us
+        Health -= damage;
+        //if health is 0, destroy the block
+        if (Health <= 0) Destroy(this.gameObject);
+    }
+
+  
+}
---
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScrolling : MonoBehaviour 
+{
+	public float ParallaxFactor;
+
+	private Vector3 previousCameraTransform;
+	
+	private Camera camera;
+	
+	private void Start () {
+        camera = Camera.main;
+        previousCameraTransform = camera.transform.position;
+	}
+
+	
+	
+	private void Update () {
+        Vector3 delta = camera.transform.position - previousCameraTransform;
+        delta.y = 0; delta.z = 0;
+        transform.position += delta / ParallaxFactor;
+
+
+        previousCameraTransform = camera.transform.position;
+	}
+
+	
+}
